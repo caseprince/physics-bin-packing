dae647934f048fd1234063f2ded68857cda9dd4a
Case Prince <caseprince@gmail.com>
2023-11-17 21:32:29 -0500

    Initial baked g.hitzones (single path)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Box2DSim from "./Box2DSim";
-import svgFile from "./svg/cat.svg";
+import svgFile from "./svg/cat_faces.svg";
 
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       .then((response) => response.text())
       .then((textContent) => {
         setSvgSource(textContent)
-        // console.log(svgSource)
+        // console.log(textContent)
       });
   })
 
--- src/Box2DSim.tsx
+++ src/Box2DSim.tsx
@@ -5,12 +5,10 @@ import { renderToString } from 'react-dom/server';
 import {
     b2Body,
     b2BodyType,
-    b2CircleShape,
     b2EdgeShape,
     b2Fixture,
     b2FixtureDef,
     b2LinearStiffness,
-    b2MakeNumberArray,
     b2MouseJoint,
     b2MouseJointDef,
     b2PolygonShape,
@@ -18,7 +16,6 @@ import {
     b2World,
     DrawJoints,
     DrawShapes,
-    DrawAABBs,
     XY,
 } from "@box2d/core";
 import { DebugDraw } from "@box2d/debug-draw";
@@ -28,10 +25,8 @@ import Stats from "stats.js";
 const ZOOM = 4;
 let SHEET_WIDTH = 384;
 let SHEET_HEIGHT = 790;
-let PADDING = 20;
 SHEET_WIDTH /= ZOOM;
 SHEET_HEIGHT /= ZOOM;
-PADDING /= ZOOM;
 
 const WIDTH = 1000;
 const HEIGHT = 1000;
@@ -41,19 +36,11 @@ interface IPartTransform { x: Number; y: Number; rotation: number }
 function Box2DSim({ svg }: { svg: string }) {
     const debugCanvasRef = useRef<HTMLCanvasElement>(null);
     const m_debugDraw = useRef<DebugDraw>();
-    // const [faceTransforms, setFaceTransforms] = useState<
-    //   IPartTransform[]
-    // >([]);
     const [paused, setPaused] = useState(false);
     const faceTransforms = useRef<IPartTransform[]>([])
 
     const stats = new Stats();
     stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-
-
-    // if (!svgSource) {
-    //   return null;
-    // }
 
     let faceGroups: NodeListOf<Element> | undefined = undefined
     let faceGeoms: {
@@ -65,58 +52,38 @@ function Box2DSim({ svg }: { svg: string }) {
 
     const parser = new DOMParser();
     const doc = parser.parseFromString(svg, "application/xml");
-    // print the name of the root element or error message
+
     const errorNode = doc.querySelector("parsererror");
     if (errorNode) {
         console.log("error while parsing");
     }
-    // } else {
-    //   console.log(doc.documentElement);
-    // }
 
     faceGroups = doc.documentElement.querySelectorAll("svg > g.face");
     faceGeoms = Array.from(faceGroups).map((faceGroup) => {
-        const paths = faceGroup.querySelectorAll("path");
-        const lines = Array.from(paths).filter((path) =>
-            path.getAttribute("d")?.includes(" L ")
-        );
+        const hitBoxPaths = faceGroup.querySelectorAll("g.hitboxes > path");
+        const dParts = hitBoxPaths[0].getAttribute("d")?.split(" ") || [];
+        /* EG: [
+            "M",
+            "30.3539465789905",
+            "-14.8089705308746",
+            "L",
+            "0.045809654247714", // Loop from index 4!
+            "-24.6159674447546",
+            "L",
+            "0.045809633959952",
+            "-1.93172136715616",
+            "L",
+            "30.3539465789905",
+            "-14.8089705308746"
+        ]*/       
+
         const polygonPoints: Array<{ x: number; y: number }> = [];
-        lines.forEach((line, i) => {
-            const dParts = line.getAttribute("d")?.split(" ") || [];
-            // Square off rounded corners (inaccurately!)
-            // if (dParts?.length) {
-            //   polygonPoints.push({ x: +dParts[1], y: +dParts[2] }); // "M"
-            //   polygonPoints.push({ x: +dParts[4], y: +dParts[5] }); // "L"
-            // }
-
-            const nextLineIndex = (i + 1) % lines.length;
-            const nextLine = lines[nextLineIndex];
-            const dParts2 = nextLine.getAttribute("d")?.split(" ") || [];
-            const intersection = intersect(
-                +dParts[1],
-                +dParts[2],
-                +dParts[4],
-                +dParts[5],
-                +dParts2[1],
-                +dParts2[2],
-                +dParts2[4],
-                +dParts2[5]
-            );
-            if (intersection) {
-                polygonPoints.push(intersection);
-            } else {
-                console.error("Parallel lines!?");
-            }
-        });
-
-        // # (rx ry angle large-arc-flag sweep-flag x y)
-        // Rounded corner circles can be too big for skinny triangles, and also kill FPS
-        // const circles = Array.from(paths)
-        //   .filter((path) => path.getAttribute("d")?.includes(" A "))
-        //   .map((path) => {
-        //     const dParts = path.getAttribute("d")?.split(" ");
-        //     return { radius: (dParts && +dParts[4]) as number };
-        //   });
+        for (let d = 4; d < dParts.length; d += 3) {
+            polygonPoints.push({ x: +dParts[d], y: +dParts[d+1] }); // "L"
+        }
+        if (polygonPoints.length > 7) {
+            console.warn("Warning! Hitzones with more than 7 control points will be truncated!")
+        }
 
         return { polygonPoints };
     });
@@ -240,18 +207,6 @@ function Box2DSim({ svg }: { svg: string }) {
         }
         // const SVG_RENDER = false; // SVG_RENDER && 
         if (bodies.some((body) => body.IsAwake())) {
-            // setFaceTransforms(
-            //   bodies.map((body) => {
-            //     const pos = body.GetPosition();
-            //     const angle = body.GetAngle();
-            //     return {
-            //       x: pos.x * ZOOM - 1500,
-            //       y: pos.y * ZOOM - 1500,
-
-            //       rotation: radiansToDegrees(angle),
-            //     };
-            //   })
-            // );
             faceTransforms.current = bodies.map((body) => {
                 const pos = body.GetPosition();
                 const angle = body.GetAngle();
@@ -355,7 +310,7 @@ function Box2DSim({ svg }: { svg: string }) {
         }
     };
 
-    console.log("render App");
+    console.log("render Box2DSim");
     return (
         <div className="App">
             <canvas ref={debugCanvasRef} width="1000" height={1000} />
@@ -438,46 +393,6 @@ const SVGOutput = ({ faceGroups, faceTransforms }: { faceGroups?: NodeListOf<Ele
         })}
     </svg>
 )
-
-// line intercept math by Paul Bourke http://paulbourke.net/geometry/pointlineplane/
-// Determine the intersection point of two line segments
-// Return FALSE if the lines don't intersect
-function intersect(
-    x1: number,
-    y1: number,
-    x2: number,
-    y2: number,
-    x3: number,
-    y3: number,
-    x4: number,
-    y4: number
-) {
-    // Check if none of the lines are of length 0
-    if ((x1 === x2 && y1 === y2) || (x3 === x4 && y3 === y4)) {
-        return false;
-    }
-
-    const denominator = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
-
-    // Lines are parallel
-    if (denominator === 0) {
-        return false;
-    }
-
-    let ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denominator;
-    // let ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denominator;
-
-    // is the intersection along the segments
-    // if (ua < 0 || ua > 1 || ub < 0 || ub > 1) {
-    //   return false;
-    // }
-
-    // Return a object with the x and y coordinates of the intersection
-    let x = x1 + ua * (x2 - x1);
-    let y = y1 + ua * (y2 - y1);
-
-    return { x, y };
-}
 
 function radiansToDegrees(radians: number) {
     return radians * (180 / Math.PI);
